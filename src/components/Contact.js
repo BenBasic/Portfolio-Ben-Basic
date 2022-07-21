460365b349b0d428825fe6b448a7cf6560892d5b
BenBasic <benjaminmbasic@gmail.com>
2022-07-20 23:34:22 -0400

    Created helper for email validation to be used within Contact.js, created the logic for Contact.js, still need to add the actual html form inside of it, will be working on that next

---
+++ src/components/Contact.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import { validateEmail } from "../utils/helpers";
+
+export default function Contact() {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e) => {
+        // Preventing the page from refreshing which is the default behaviour of form submit (which is what this will be used for later)
+        e.preventDefault();
+        // Setting the formState to empty values so that the form clears after being submitted
+        setFormState({ ...formState,
+        name: "",
+        email: "",
+        message: "",
+    });
+    };
+
+    const handleChange = (e) => {
+        // If the selected field is email, then it will perform a validation from the helper, it will then set the error message based on if the validation passed or not
+        if (e.target.name === "email") {
+            const validCheck = validateEmail(e.target.value);
+            if (!validCheck) {
+                setErrorMessage("Please enter a valid email")
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+            // If there is nothing entered in either name or message fields when theyre selected, it will set the error based on if the field is empty
+            if (!e.taget.value.length) {
+                setErrorMessage(`Please enter a ${e.target.name}`)
+            } else {
+                setErrorMessage("");
+            }
+        }
+        if (!errorMessage) {
+            // If there is a valid email, and something inside of the name and message fields, it will assign the values from the form to the appropriate properties for the formState
+            setFormState({ ...formState, [e.target.name]: e.target.value});
+            console.log("Form data check", formState)   
+        }
+    };
+
+
+}
