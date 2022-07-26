c29641f1d96dbd206928b732af29cf7c4a05e680
BenBasic <benjaminmbasic@gmail.com>
2022-07-26 02:01:01 -0400

    Made some minor tweaks to the About page and styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div className='container'>
+        <div className='container aboutContainer'>
             <div className='mx-5 mt-5 p3 aboutPage'>
                 <h1 className='text-center'>About Me</h1>
                 <div className='row'>
                 <div className="col-md-2">
                     <img className='profilePicture' src={require('./assets/images/me.png')} alt='Benjamin Basic smiling' />
                 </div>
-                <div className="col-md-10 testClass">
-                    <p className='testClass2'>test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at test at </p>
+                <div className="col-md-10 bioContainer">
+                    <p className='bio'>My name is Ben Basic. Im 27 years old and I've been a 2D animator for 17 of those years. Animation might not seem too similar to programming, but they both involve staying up until 4 in the morning fixing something that almost nobody is going to notice, so programming fits right in my comfort zone.  I'm currently attending the University of Toronto's programming bootcamp, where I hope to create things that both my peers and potential employers find value in by the time I graduate.</p>
                     <div className='row'>
                         <div className='col-md-6 contactDetails'>
                             
