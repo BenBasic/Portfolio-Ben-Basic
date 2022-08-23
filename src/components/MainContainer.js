10fa44a47823faf681d5b62ed073578d6908a6bc
BenBasic <benjaminmbasic@gmail.com>
2022-08-23 17:29:25 -0400

    Implemented spritesheet animations as buttons. When one button is clicked, the button's animation will play. It will only play once (as intended), and currently it will not play other button animations once another button has already been pressed (also intended). This is the first stage in my redesign for the portfolio site, my plan is to make the initial page the user is presented to be 4 large buttons which will re-organize/disappear from the page once clicked and loading the related content. Will continue to work on this

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,6 +5,7 @@ import About from './About';
 import Portfolio from './Portfolio';
 import Contact from './Contact';
 import Resume from './Resume';
+import Spritesheet from 'react-responsive-spritesheet';
 
 export default function MainContainer() {
     const [currentPage, setCurrentPage] = useState('About');
@@ -25,11 +26,137 @@ export default function MainContainer() {
 
     const handlePageChange = (page) => setCurrentPage(page);
 
+    // const canvas = document.getElementsByClassName("spritesheetCanvas");
+    // const context = canvas.getContext("2d");
+    // const width = 351;
+    // const height = 351;
+    // const spriteRow = 0;
+    // const spriteColumn = 0;
+    // const spriteFrameSize = 351;
+    // const spritesheetWidth = 2106;
+    // const spritesheetHeight = 1755;
+    // const fps = 24;
+    // const xPos = 351;
+    // const yPos = 351;
+    // const scale = 1;
+    // const timeToUpdate = 1 * fps;
+    // let frameNumber = 0;
+
+    // const spritesheet = new Image();
+    // spritesheet.src = "./assets/images/PortfolioArtAboutMe.png"
+
+    // function animate() {
+    //     context.drawImage(
+    //         spritesheet,
+    //         0, // First pixel on x axis
+    //         0, // First pixel on y axis
+    //         spriteFrameSize, // Frame width
+    //         spriteFrameSize, // Frame height
+    //         xPos, // x position to draw on canvas
+    //         yPos, // y position to draw on canvas
+    //         spriteFrameSize * scale, // Desired width to draw on canvas (size of frame multiplied be desired scale)
+    //         spriteFrameSize * scale, // Desired height to draw on canvas (size of frame multiplied be desired scale)
+
+    //     )
+    // }
+
+    // function frame() {
+    //     context.clearRect(0, 0, width, height);
+    //     animate();
+    //     requestAnimationFrame(frame);
+    // }
+
+    // frame();
+
+    const [clickedState, setClickedState] = useState(false);
+    //let clicked = false;
+    console.log("CLICKED IS")
+    console.log(clickedState)
+
+
     return (
-        <div className="mainContainer">
-            <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-            <NavBarMobile currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
-        </div>
+        <>
+            <Spritesheet
+                className={`aboutMeAnimation`}
+                image={require('./assets/images/PortfolioArtAboutMe.png')}
+                widthFrame={351}
+                heightFrame={351}
+                steps={26}
+                fps={24}
+                autoplay={false}
+                loop={false}
+                onClick={clickedState ?
+                    spritesheet => {
+                        spritesheet.pause();
+                    }
+                    :
+                    spritesheet => {
+                        spritesheet.play();
+                        setClickedState(true);
+                    }}
+            />
+            <Spritesheet
+                className={`portfolioAnimation`}
+                image={require('./assets/images/PortfolioArtPortfolio.png')}
+                widthFrame={351}
+                heightFrame={351}
+                steps={34}
+                fps={24}
+                autoplay={false}
+                loop={false}
+                onClick={clickedState ?
+                    spritesheet => {
+                        spritesheet.pause();
+                    }
+                    :
+                    spritesheet => {
+                        spritesheet.play();
+                        setClickedState(true);
+                    }}
+            />
+            <Spritesheet
+                className={`contactAnimation`}
+                image={require('./assets/images/PortfolioArtContact.png')}
+                widthFrame={351}
+                heightFrame={351}
+                steps={22}
+                fps={24}
+                autoplay={false}
+                loop={false}
+                onClick={clickedState ?
+                    spritesheet => {
+                        spritesheet.pause();
+                    }
+                    :
+                    spritesheet => {
+                        spritesheet.play();
+                        setClickedState(true);
+                    }}
+            />
+            <Spritesheet
+                className={`resumeAnimation`}
+                image={require('./assets/images/PortfolioArtResume.png')}
+                widthFrame={351}
+                heightFrame={351}
+                steps={19}
+                fps={24}
+                autoplay={false}
+                loop={false}
+                onClick={clickedState ?
+                    spritesheet => {
+                        spritesheet.pause();
+                    }
+                    :
+                    spritesheet => {
+                        spritesheet.play();
+                        setClickedState(true);
+                    }}
+            />
+            <div className="mainContainer">
+                <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+                <NavBarMobile currentPage={currentPage} handlePageChange={handlePageChange} />
+                {renderPage()}
+            </div>
+        </>
     );
 };
